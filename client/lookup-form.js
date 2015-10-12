6cfa2c247f52857db7faf356f048bcdf0f134702
casteezy <tricx3illo@gmail.com>
2015-10-11 22:05:29 -0700

    New service provider.

--- client/lookup-form.js
+++ client/lookup-form.js
@@ -1,26 +1,21 @@
-var _itemId = '075691002589';
-
 angular
     .module('amazonLookup', [
         'angular-meteor'
     ])
-    .controller('lookupCtrl', ['$scope', '$http', '$meteor',
-        function ($scope, $http, $meteor) {
-            $scope.title = 'Amazon Item Lookup by UPC';
-            $scope.results = '';
-            $scope.codePrint = {};
-            $scope.items = [];
+    .controller('lookupCtrl', ['$scope', '$http', '$meteor', 'SearchService',
+        function($scope, $http, $meteor, SearchService) {
+            var _itemId = '075691002589';
+
+            //$scope.results = '';
+            $scope.codePrint = {}; // for debugging
 
             $scope.resultItems = $meteor.collection(ResultItems);
 
-            $scope.submit = function () {
+            $scope.submit = function() {
                 var itemId = _itemId;
                 $meteor.call('searchByItemId', itemId).then(
                     function success(data) {
-                        console.log('Successful! ' + data);
-                        if (data) {
-                            $scope.codePrint['Callback Results'] = data;
-                        }
+                        console.log('Successful!');
                         if ($scope.resultItems.length === 0) {
                             $scope.codePrint['Result Items?'] = false;
                         } else {
@@ -35,56 +30,72 @@ angular
             }
 
             function debugFormat() {
-                transformObjArray($scope.resultItems[0]);
+                //transformObjArray($scope.resultItems[0]);
                 $scope.resultItems.forEach(function (obj, idx, arr) {
                     angular.forEach(obj, function (value, key) {
                         $scope.codePrint[key] = value;
                     });  // for debugging
+
+                    //$scope.items = [];
                     //obj.Items && obj.Items.forEach(function (itemWrapper, indx, array) {
-                        //angular.forEach(itemWrapper.Item, function (val, key) {
-                        //    $scope.items.push(getItemData(val));
-                        //});
+                    //angular.forEach(itemWrapper.Item, function (val, key) {
+                    //    $scope.items.push(getItemData(val));
+                    //});
                     //});
                 });
             }
 
+            var valuesToFind = {
+                name: ['Item', 'ItemAttributes', 'Title'],
+                listPrice: ['Item', 'ItemAttributes', 'ListPrice', 'FormattedPrice'],
+                department: ['Item', 'ItemAttributes', 'Department'],
+                productGroup: ['Item', 'ItemAttributes', 'ProductGroup'],
+                productType: ['Item', 'ItemAttributes', 'ProductTypeName'],
+                newCount: ['Item', 'OfferSummary', 'TotalNew'],
+            };
             $scope.clicked = function () {
                 var resultItems = $scope.resultItems[0]['Items'];
-                var valuesToFind = {
-                    name: [ 'Item', 'ItemAttributes', 'Title' ],
-                    listPrice: [ 'Item', 'ItemAttributes', 'ListPrice', 'FormattedPrice' ],
-                    department: [ 'Item', 'ItemAttributes', 'Department' ],
-                    productGroup: [ 'Item', 'ItemAttributes', 'ProductGroup' ],
-                    productType: [ 'Item', 'ItemAttributes', 'ProductTypeName' ],
-                    newCount: [ 'Item', 'OfferSummary', 'TotalNew' ],
-                }
 
+                var foundValues = {};
                 for (var val in valuesToFind) {
-                    $scope.codePrint[val] = find(resultItems, valuesToFind[val]);
+                    foundValues[val] = SearchService.findValue(resultItems, valuesToFind[val]);
                 }
+                $scope.results = SearchService.convertToCsvInternal(foundValues);
             };
 
-            /**
-             * @param searchObj object to search through
-             * @param searchPropList list of properties to go through to get to value (hierarchy)
-             * @returns found value
-             */
-            function find(searchObj, searchPropList) {
-                if (!searchObj) { // Result not found
-                    return;
-                }
-                if (searchPropList.length === 0) { // Result found
-                    if (Array.isArray(searchObj) && searchObj.length === 1) {
-                        return searchObj[0];
-                    }
-                    return searchObj;
-                }
-                var property = searchPropList.splice(0, 1); // Remove first object
-                if (Array.isArray(searchObj)) { // Keep searching thru array-wrapped object
-                    return find(searchObj[0][property], searchPropList);
-                }
-                // Keep searching thru object
-                return find(searchObj[property], searchPropList);
-            }
+        }]) // end controller
 
-        }]); // end controller,
+    .service('SearchService', [function() {
+        /**
+         * @param searchObj object to search through
+         * @param searchPropList list of properties to go through to get to value (hierarchy)
+         * @returns found value in object
+         */
+        function findValueInternal(searchObj, searchPropList) {
+            if (!searchObj) { // Result not found
+                return null;
+            }
+            if (searchPropList.length === 0) { // Result found
+                if (Array.isArray(searchObj) && searchObj.length === 1) {
+                    return searchObj[0];
+                }
+                return searchObj;
+            }
+            var firstProperty = searchPropList.splice(0, 1)[0]; // Remove first object
+            if (Array.isArray(searchObj)) { // Keep searching thru array-wrapped object
+                return findValueInternal(searchObj[0][firstProperty], searchPropList);
+            }
+            // Keep searching thru object
+            return findValueInternal(searchObj[firstProperty], searchPropList);
+        }
+
+        // TODO
+        function convertToCsvInternal(obj) {
+            return JSON.stringify(obj); // temporary
+        }
+
+        return {
+            findValue: findValueInternal,
+            convertToCsv: convertToCsvInternal
+        };
+    }]); // end service
